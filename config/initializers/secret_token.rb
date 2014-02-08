ac767d73b1677fde7168aa05dda2a32615a3668a
rskvirskiy <rskvirskiy@gmail.com>
2014-02-08 18:36:17 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeachersJournal::Application.config.secret_key_base = '41be84c5933f050ead02a652111b0f992a62bdfee5670fb5336b183f5aca3636285a885d5e587fec172be6817a53771df11a9175343b0ee378bd0758f8aef7f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeachersJournal::Application.config.session_store :cookie_store, key: '_teachers_journal_session'
